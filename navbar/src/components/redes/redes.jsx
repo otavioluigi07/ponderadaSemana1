05fe4f2d0a00eb651456dfc702703042bc368594
otavioluigi07 <luigi.macedo@sou.inteli.edu.br>
2024-02-08 20:57:14 -0300

    Seção '''O que são as redes?'''

---
+++ navbar/src/components/redes/redes.jsx
@@ -0,0 +1,34 @@
+import styles from './styles.module.scss'
+
+
+function Redes() {
+    return (
+    <div className={styles.Redes}>
+        <h1>O que são as redes sociais? 😊</h1>
+        <div className={styles.texto}>
+            <p>
+            As redes sociais são plataformas online que permitem que pessoas se conectem, interajam e compartilhem informações em um ambiente virtual. Essas plataformas proporcionam um espaço digital para a criação de perfis pessoais ou empresariais, nos quais os usuários podem compartilhar textos, imagens, vídeos e outros tipos de conteúdo com seus contatos ou com o público em geral.
+
+            O principal objetivo das redes sociais é facilitar a comunicação e a interação entre indivíduos, independentemente da distância geográfica que os separa. Por meio dessas plataformas, é possível manter contato com amigos, familiares e colegas, além de fazer novas amizades e estabelecer conexões profissionais.
+
+            Dentre as características comuns das redes sociais, estão os recursos de curtir, comentar e compartilhar conteúdo, o que permite que os usuários expressem suas opiniões, gostos e interesses de maneira rápida e interativa. Além disso, as redes sociais são frequentemente utilizadas como ferramentas de divulgação, marketing e networking, tanto para indivíduos quanto para empresas.
+
+            As redes sociais têm desempenhado um papel significativo na transformação da forma como as pessoas se comunicam e consomem informações. Elas proporcionam uma plataforma para o compartilhamento instantâneo de notícias, eventos e experiências, contribuindo para a formação de comunidades virtuais e para a disseminação rápida de informações.
+
+            No entanto, é importante destacar que o uso das redes sociais também traz consigo desafios, como questões relacionadas à privacidade, à disseminação de desinformação e à gestão do tempo. Portanto, é fundamental que os usuários estejam cientes dessas questões e utilizem as redes sociais de maneira consciente e responsável.</p>
+        </div>
+
+
+
+
+    </div>
+
+
+
+
+
+
+    );
+  }
+  
+  export default Redes;
